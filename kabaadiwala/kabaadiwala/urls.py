2706a4f9561b1ab55c4d0c570470545d5f8c7b49
Anuj Chourasiya <anujchourasiyadp@gmail.com>
2021-03-26 13:33:42 +0530

    home app added

--- kabaadiwala/kabaadiwala/urls.py
+++ kabaadiwala/kabaadiwala/urls.py
@@ -20,7 +20,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('home.urls')),
     path('account/',include('account.urls')),
+    path('',include('account.urls')),
     path('kabaad/',	include('addkabaad.urls',	namespace='kabaad')),
     path('cart/',include('shopping_cart.urls',namespace='cart')),
 ]
