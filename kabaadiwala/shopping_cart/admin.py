8741de67215464b9f7d82998f2ff83bf79e5fdfc
Anuj Chourasiya <anujchourasiyadp@gmail.com>
2021-04-16 17:44:19 +0530

    shipping model registered

--- kabaadiwala/shopping_cart/admin.py
+++ kabaadiwala/shopping_cart/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import OrderItem, Order, Transaction
+from .models import OrderItem, Order,ShippingAddress
 
 admin.site.register(OrderItem)
 admin.site.register(Order)
-admin.site.register(Transaction)
+admin.site.register(ShippingAddress)
