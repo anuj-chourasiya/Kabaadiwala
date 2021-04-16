1db937e7eef0100bc11916da3445677504140ec6
Anuj Chourasiya <anujchourasiyadp@gmail.com>
2021-04-16 17:46:53 +0530

    url pattern added for order processing

--- kabaadiwala/shopping_cart/urls.py
+++ kabaadiwala/shopping_cart/urls.py
@@ -4,6 +4,8 @@ from .views import (
     add_to_cart,
     delete_from_cart,
     order_details,
+    checkout,
+    processOrder,
    
 )
 
@@ -13,5 +15,7 @@ urlpatterns = [
     url(r'^add-to-cart/(?P<item_id>[-\w]+)/$', add_to_cart, name="add_to_cart"),
     url(r'^order-summary/$', order_details, name="order_summary"),
     url(r'^item/delete/(?P<item_id>[-\w]+)/$', delete_from_cart, name='delete_item'),
+    url(r'^checkout/$', checkout, name='checkout'),
+    url(r'process_order/',processOrder, name='process_order'),
     
 ]
